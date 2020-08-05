4a70cbcecee52e46076b4aa32fbcce063d57e8b3
Litvinov Yurka <litvinov.yuriy1996@gmail.com>
2020-08-05 22:50:37 +0400

    init(project): all init

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
+let vh;
 $(document).ready(function () {
+    vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${ vh }px`);
+
     $('.country__item').on('touchstart', function (e) {
         changeActive(e, this, 'country');
     });
@@ -6,6 +11,12 @@ $(document).ready(function () {
     $('.sfier__item').on('touchstart', function (e) {
         changeActive(e, this, 'sfier');
     });
+});
+
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
 function changeActive(e, current, selector) {
